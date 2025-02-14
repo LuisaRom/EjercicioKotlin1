6dd060d16110c5483bfc11d6d0fa6368323b94bc
LuisaRom <lufromero13@gmail.com>
2025-02-14 13:43:30 -0500

    Salidad por consola

--- src/Main.kt
+++ src/Main.kt
@@ -3,6 +3,10 @@ fun main() {
 
     println("Hola Mundo")
     println("Primer ejercicio")
+    println("Primer ejercicio")
+    println("Primer ejercicio")
+    println("Primer ejercicio")
+    println("Primer ejercicio")
 
 
 }
